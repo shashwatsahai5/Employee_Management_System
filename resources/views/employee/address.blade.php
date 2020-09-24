ac7334511749730c1ad542a65d72d928c6be09ce
Shashwat Sahai <shashwatsahai5@gmail.com>
2020-09-25 04:31:08 +0530

    added dynamic dropdowns for country and state names

--- resources/views/employee/address.blade.php
+++ resources/views/employee/address.blade.php
@@ -34,26 +34,34 @@
                 {{Form::label('address_type', 'Address Type')}}
                 {{Form::text('address_type','' , ['class' => 'form-control', 'placeholder' => 'Eg. Home, Work, Hostel, PG'])}}
             </div>
+            
+            
+            
             <div class = "form-group">
-                {{Form::label('house_number', 'House Number')}}
-                {{Form::text('house_number','' , ['class' => 'form-control', 'placeholder' => 'House Number'])}}
+                {{Form::label('country', 'Country')}}
+                <select class = "form-control" id = "countryList" name = "country"></select>
             </div>
+
+            <div class = "form-group">
+                {{Form::label('state', 'State')}}
+                <select class = "form-control" id = "stateList" name = "state"></select>
+            </div>
+
+            <div class = "form-group">
+                {{Form::label('city', 'City')}}
+                {{Form::text('city', '' , ['class' => 'form-control', 'placeholder' => 'City'])}}
+            </div>
+            
             <div class = "form-group">
                 {{Form::label('street', 'Street')}}
                 {{Form::text('street',  '' ,['class' => 'form-control', 'placeholder' => 'Street'])}}
             </div>
             <div class = "form-group">
-                {{Form::label('city', 'City')}}
-                {{Form::text('city', '' , ['class' => 'form-control', 'placeholder' => 'City'])}}
+                {{Form::label('house_number', 'House Number')}}
+                {{Form::text('house_number','' , ['class' => 'form-control', 'placeholder' => 'House Number'])}}
             </div>
-            <div class = "form-group">
-                {{Form::label('state', 'State')}}
-                {{Form::text('state',  '' ,['class' => 'form-control', 'placeholder' => 'State'])}}
-            </div>
-            <div class = "form-group">
-                {{Form::label('country', 'Country')}}
-                {{Form::text('country','' , ['class' => 'form-control', 'placeholder' => 'Country'])}}
-            </div>
+
+
             <div class = "form-group">
                 {{Form::label('zip', 'Zip')}}
                 {{Form::number('zip','' , ['class' => 'form-control', 'placeholder' => 'zipcode'])}}
@@ -73,6 +81,50 @@
     <script type="text/javascript">
         function showDiv() {
             document.getElementById('newAddressForm').style.display = "block";
+            var countries = new XMLHttpRequest()
+            var BATTUTA_KEY="026fb43d98da8f4a8bf0a1630b78009b"
+            url="http://battuta.medunes.net/api/country/all/?key="+BATTUTA_KEY;
+            countries.open('GET',url,true)
+            var list = {};
+            countries.onload = function(){
+                var data = JSON.parse(this.response)
+                data.forEach((c)=>{
+                    
+                    list[c.name] = c.code;
+                    console.log(list[c.name]);
+                    let newOption = new Option(c.name,c.name);
+                    countryList.add(newOption,undefined);
+                })
+            }
+
+            countries.send()
+            let select = document.querySelector('#countryList');
+            select.addEventListener('change', function () {
+                var cntry = list[this.value];
+                console.log("you selected: " + list[this.value]);
+                url2="http://battuta.medunes.net/api/region/"+cntry+"/all/?key="+BATTUTA_KEY;
+                var states = new XMLHttpRequest()
+                states.open('GET',url2,true)
+                //var list2 = {};
+                states.onload = function(){
+                    var selectobject = document.getElementById("stateList");
+                    for (var i=0; i<selectobject.length; i++) {
+                        selectobject.remove(i);
+                        i--;
+                    }
+                    var data2 = JSON.parse(this.response)
+                    data2.forEach((s)=>{
+                        //list2[s.name] = s.code;
+                        console.log(s.region);
+                        let newOption2 = new Option(s.region,s.region);
+                        stateList.add(newOption2,undefined);
+                    })
+                }
+                states.send()
+
+
+            });
+
         }
     </script>
 </div>
